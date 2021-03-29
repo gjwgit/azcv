4dafb991aeb19cf6b471e0a3c7bfde0c610b8811
jingjinggit config --global user.email 745287546@qq.com <745287546@qq.com>
2021-03-29 15:14:59 +1100

    Use try-except to catch the image url error in category method

--- category.py
+++ category.py
@@ -13,7 +13,9 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 
 import os
 import argparse
-from textwrap import fill
+import urllib.error
+import urllib.request
+import sys
 
 from mlhub.pkg import azkey, is_url
 from mlhub.utils import get_cmd_cwd
@@ -62,7 +64,23 @@ path = args.path
 image_features = ["categories"]
 
 if is_url(path):
-    analysis = client.analyze_image(path, image_features)
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        req = urllib.request.Request(path, headers=headers)
+
+        if urllib.request.urlopen(req).status == 200:
+            try:
+                analysis = client.analyze_image(path, image_features)
+            except Exception:
+                print("Error: Image URL is not accessible")
+                print(path)
+                sys.exit(1)
+
+    except urllib.error.URLError:
+        print("Error: Image URL is not available.")
+        print(path)
+        sys.exit(1)
+
 else:
     path = os.path.join(get_cmd_cwd(), path)
     with open(path, 'rb') as fstream:
