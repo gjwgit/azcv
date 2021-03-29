3630c78259852e8703e3c3116c5859ebc81ad0db
jingjinggit config --global user.email 745287546@qq.com <745287546@qq.com>
2021-03-29 16:07:05 +1100

    Use try-except to catch the url error in ocr method

--- ocr.py
+++ ocr.py
@@ -73,7 +73,12 @@ if is_url(url):
         print(f"The URL does not appear to exist. Please check.")
         print(f"    {url}")
         quit()
-    rawHttpResponse = client.read(url, raw=raw)
+    try:
+        rawHttpResponse = client.read(url, raw=raw)
+    except Exception as e:
+        print(f"Error: {e}\n{url}")
+        quit()
+
 else:
     path = os.path.join(get_cmd_cwd(), url)
     with open(path, 'rb') as fstream:
